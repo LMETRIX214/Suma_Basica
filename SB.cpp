ff7a5d960caf5b9177ae71e5ac53c1eceb58f8b6
marco <lmetrix214@gmail.com>
2021-05-22 15:13:50 -0500

    Suma Basica

---
+++ SB.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+main(){
+	system("title Suma Basica");
+	float n1,n2,r;
+	printf("\n Ingrese un numero: ");
+	scanf("%f",&n1);
+	printf("\n Ingrese un numero: ");
+	scanf("%f",&n2);
+	r=n1+n2;
+	printf("\n La respuesta es: %f",r);
+}
